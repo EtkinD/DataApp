ea87a25ffc7b3803b07effd850bd68bff8ab06f0
EtkinD <etkin.dogan@hotmail.com>
2025-01-09 15:52:12 +0300

    express init

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
+// ========== Environment Setup ==========
 import { config } from "dotenv";
 config();
 
+// ========== ExpressJS Setup ==========
+import {
+    applyMiddleWare,
+    configureRoutes,
+    startServer
+} from "./src/express";
+
+applyMiddleWare(process.env.STATIC_DIR);
+configureRoutes();
+startServer(Number(process.env.PORT), String(process.env.HOST));
---
+++ src/express/index.ts
@@ -0,0 +1,37 @@
+import express, { } from 'express';
+
+const app = express();
+
+/**
+ * Applies the middleware to the express app
+ * @param publicFolder The folder where the static files are located
+ */
+export function applyMiddleWare(publicFolder?: string): void {
+    // To be able to read the body of the request
+    app.use(express.json());
+
+    // Serve static files if the public folder is provided
+    if (publicFolder)
+        app.use(express.static(publicFolder));
+}
+
+/**
+ * Configures the routes for the express app
+ */
+export function configureRoutes(): void {
+    // TODO: temporarily added to test the server
+    app.get('/hello', (req, res) => {
+        res.send('Hello World!');
+    });
+}
+
+/**
+ * Starts the express server on the specified port and host
+ * @param port port number
+ * @param host host name
+ */
+export function startServer(port: number, host: string): void {
+    app.listen(port, host, () => {
+        console.log(`[HTTP][S] Server started at http://${host}:${port}`);
+    });
+}
