510f3eb55ad837af701e4ef37cb68a5d78962c47
EtkinD <etkin.dogan@hotmail.com>
2025-01-10 16:53:44 +0300

    mocha unit tests library added

---
+++ test/index.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Sample Tests', function () {
+    it('First Sample', function () {
+        expect(true).to.equal(true);
+    });
+
+    it('Second Sample', function () {
+        expect(true).to.equal(false);
+    });
+});
