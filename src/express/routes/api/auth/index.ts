209d3bfcb0c3a1300ae32c9fe47d9cb1cf01a2ec
EtkinD <etkin.dogan@hotmail.com>
2025-01-11 14:30:13 +0300

    Change login and register routes to POST and add temporary user storage

--- src/express/routes/api/auth/index.ts
+++ src/express/routes/api/auth/index.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import login from "./login";
 import register from "./register";
+import { UserEntity } from "../../../../types/db/user";
 
 // PATH: /api/auth
 const router = Router();
 
-router.get('/login', login);        // PATH: /api/auth/login
-router.get('/register', register);  // PATH: /api/auth/register
+router.post('/login', login);        // PATH: /api/auth/login
+router.post('/register', register);  // PATH: /api/auth/register
 
 export default router;
+
+// TODO: Remove this temporary object after implementing database
+// Temporary object to store user data
+const users: Array<UserEntity> = [];
+export { users };
