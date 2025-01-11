cee3ffc11ac157c66a352bd08e2899c12b367700
EtkinD <etkin.dogan@hotmail.com>
2025-01-11 14:30:27 +0300

    Implement login and register functionality with validation and token generation

--- src/express/routes/api/auth/login.ts
+++ src/express/routes/api/auth/login.ts
@@ -1,7 +1,56 @@
 import { Request, Response } from "express";
+import { LoginRequest } from "../../../../types/request/auth";
+import { AuthResponseFactory } from "../../../../types/response/auth";
+import { compare } from "../../../../util/hashing";
+import { users } from ".";
+import { generateToken } from "../../../../util/jwt";
 
 function login(req: Request, res: Response): void {
-    res.send('GET ~ /api/auth/login route works');
+    const reqbody = req.body as LoginRequest;
+
+    if (!reqbody.username || !reqbody.password) {
+        const r = AuthResponseFactory()
+            .setStatus(400)
+            .setSuccess(false)
+            .setMessage('Missing username or password');
+
+        res.status(400).send(r);
+        return;
+    }
+
+    const user = users.find((user) => user.username === reqbody.username);
+
+    if (!user) {
+        const r = AuthResponseFactory()
+            .setStatus(400)
+            .setSuccess(false)
+            .setMessage('User not found');
+
+        res.status(400).send(r);
+        return;
+    }
+
+    if (!compare(reqbody.password, user.password)) {
+        const r = AuthResponseFactory()
+            .setStatus(400)
+            .setSuccess(false)
+            .setMessage('Incorrect username or password');
+
+        res.status(400).send(r);
+        return;
+    }
+
+    // Update user info
+    user.lastLogin = new Date();
+    user.lastActivity = new Date();
+
+    // Send response
+    const r = AuthResponseFactory()
+        .setStatus(200)
+        .setSuccess(true)
+        .setMessage('Login successful')
+        .setToken(generateToken(user as object));
+    res.status(200).send(r);
 }
 
 export default login;
--- src/express/routes/api/auth/register.ts
+++ src/express/routes/api/auth/register.ts
@@ -1,7 +1,56 @@
 import { Request, Response } from "express";
+import { RegisterRequest } from "../../../../types/request/auth";
+import { AuthResponseFactory } from "../../../../types/response/auth";
+import { generateToken } from "../../../../util/jwt";
+import { hash } from "../../../../util/hashing";
+import { users } from ".";
+import { UserEntity } from "../../../../types/db/user";
 
 function register(req: Request, res: Response): void {
-    res.send('GET ~ /api/auth/register route works');
+    const reqbody = req.body as RegisterRequest;
+
+    if (!reqbody.email || !reqbody.password || !reqbody.username || !reqbody.name || !reqbody.lastName) {
+        const r = AuthResponseFactory()
+            .setStatus(400)
+            .setSuccess(false)
+            .setMessage('Missing fields');
+
+        res.status(400).send(r);
+        return;
+    }
+
+    const user = users.find((user) => user.username === reqbody.username);
+
+    if (user) {
+        const r = AuthResponseFactory()
+            .setStatus(400)
+            .setSuccess(false)
+            .setMessage('User already exists');
+
+        res.status(400).send(r);
+        return;
+    }
+
+    // Create new user
+    const newUser = reqbody as UserEntity;
+    // Set user info
+    newUser.id = users.length + 1;
+    newUser.password = hash(newUser.password);
+
+    newUser.joinDate = new Date();
+    newUser.lastLogin = new Date();
+    newUser.lastActivity = new Date();
+
+    // Add user to the list
+    users.push(newUser);
+
+    // Send response
+    res.status(200)
+        .send(AuthResponseFactory()
+            .setStatus(200)
+            .setSuccess(true)
+            .setMessage('User registered')
+            .setToken(generateToken(newUser as object)));
 }
 
 export default register;
